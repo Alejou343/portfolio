e7fcc37a26636ec13976045ff2b030513bd188ea
Alejou342 <alejandro.alvarez@cpocket.global>
2024-02-27 13:08:00 -0500

    Feat: Modal container added

---
+++ src/Containers/Modal/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Image from 'next/image'
+import './index.css'
+
+type ModalTypes = {
+    children: JSX.Element;
+    state: boolean;
+    setState: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Index = ({ children, state, setState}: ModalTypes) => {
+
+    return (
+        <>
+            { state &&
+                <div className='modal-general z-50'>
+                    <main className='relative modal shadow-lg bg-auxiliar p-6'>
+                        <button 
+                            className='close-modal-icon cursor-pointer'
+                            onClick={() => setState(!state)}
+                        > 
+                            <Image src='/assets/close-icon.svg' width={25} height={25} alt='cerrar' />
+                        </button>
+                        <div className='h-full w-full px-12'>
+                            {children}
+                        </div>
+                    </main>
+                </div>
+            }
+        </>
+    )
+}
+
+export default Index
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,24 +1,36 @@
 "use client";
 import React from "react";
 import { Button } from "../ui/moving-border";
+import ModalGeneral from '@/Containers/Modal';
+import Information from '@/components/Information';
 
 const index = () => {
+
+  const [openModal, setOpenModal] = React.useState<boolean>(false);
+
   return (
-    <div className="flex justify-between mt-[5rem] gap-[23rem]">
-      <h1 className="text-3xl">
-        Materials Engineer
-      </h1>
+    <>
+      {openModal && 
+      <ModalGeneral state={openModal} setState={setOpenModal}>
+        <Information />
+      </ModalGeneral>}
+      <div className="flex justify-between mt-[5rem] gap-[23rem]">
+        <h1 className="text-3xl">
+          Materials Engineer
+        </h1>
         <Button
+        onClick={() => setOpenModal(true)}
         borderRadius="1.75rem"
         className="bg-white dark:bg-slate-900 text-black dark:text-white mx-auto border-neutral-200 dark:border-slate-800 font-extrabold"
         >
             About Me
         </Button>
-      <h1 className="text-3xl">
-        Software developer
-      </h1>
-    </div>
+        <h1 className="text-3xl">
+          Software developer
+        </h1>
+      </div>
+    </>
   )
-}
+};
 
-export default index
+export default index;
